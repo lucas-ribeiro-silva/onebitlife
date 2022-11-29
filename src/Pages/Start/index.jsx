ea0c480c013b079c087f9693c59bbcce67e84d5a
Lucas Ribeiro <107866855+lucas-ribeiro-silva@users.noreply.github.com>
2022-11-29 14:37:19 -0300

    Estilização da página Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,17 +1,39 @@
-import react  from "react";
-import { View, Text, Image, ScrollView} from "react-native";
+import React from "react";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
 
 export default function Start() {
-    return (
-        <View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    <image source={require("../../assets/icons/logo3.png")} />
-                    <Text>
-                        Vamos transformar a sua vida {"/n"} em jogo, buscando sempre {"/n"}{" "} o melhor nível. 
-                    </Text>
-                </View>
-            </ScrollView>
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{alignItems: "center"}}>
+          <Image source={require("../../assets/icons/logo3.png")} 
+          style={styles.logo}
+          />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
+          </Text>
         </View>
-    )
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    },
+})
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,5 @@
 import AllPages from "./AllPages";
 
-export default function Routes(){
-    return <AllPages/>;
+export default function Routes() {
+  return <AllPages />;
 }
-
