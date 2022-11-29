3aaca272f7a3b159c738fb6ef944026da390bded
Lucas Ribeiro <107866855+lucas-ribeiro-silva@users.noreply.github.com>
2022-11-29 15:18:02 -0300

    Inserindo animação - LifeStatus e estruturando componentes

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+
+import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
-        <View style={{alignItems: "center"}}>
-          <Image source={require("../../assets/icons/logo3.png")} 
-          style={styles.logo}
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
           />
+          <LifeStatus />
           <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
           </Text>
@@ -19,21 +23,20 @@ export default function Start() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "rgba(21, 21, 21, 0.98)",
-    },
-
-    logo: {
-        width: 300,
-        height: 60,
-        marginTop: 60,
-        marginBottom: 20,
-    },
-    description: {
-        color: "#FFFFFF",
-        fontSize: 20,
-        textAlign: "center",
-        marginVertical: 60,
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
