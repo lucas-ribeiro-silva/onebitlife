d7e434366960a0917aa1b8b8905f8f41045974db
Lucas Ribeiro <107866855+lucas-ribeiro-silva@users.noreply.github.com>
2022-12-01 09:40:49 -0300

    Explanation card terminado

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,54 +1,63 @@
 import React from "react";
-import { View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import DeafultButton from "../../Components/Common/DefaultButton";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/Explanation Card";
 
-export default function AppExplanation (){
-    function handleSetShowHome() {
-        console.log("Botão cliclado");
-    }
-    return (
-        <View style={StyleSheet.container}>
-            <ScrollView>
-                <View style={{alignItems: "center"}}/>
-                    <Text style={styles.title}>
-                        Antes, deixa {"\n"} eu te explicar...
-                    </Text>
-                    <Text style={styles.description}>
-                        Pronto (a) para subir de nível na vida? 
-                    </Text>
-                    <Text style={styles.description}>
-                        Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos de forma individual.
-                    </Text>    
-                    <DeafultButton buttonText={"Continuar"} handlePress={handleSetShowHome} width={250} height={50}/>
-            </ScrollView>
-         </View>   
-    );
+export default function AppExplanation() {
+
+	function handleSetShowHome() {
+    console.log("Botão clicado!");
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.title}>
+            Antes, deixa {"\n"} eu te explicar...
+          </Text>
+					<ExplanationCard />
+          <Text style={styles.descriptionCta}>
+            Pronto(a) para subir de nível na vida?
+          </Text>
+          <Text style={styles.description}>
+            Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos de
+            forma individual.
+          </Text>
+          <DefaultButton
+            buttonText={"Continuar"}
+            handlePress={handleSetShowHome}
+            width={250}
+            height={50}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
-const styles = StyleSheet.creater ({
-    container: {
-        flex: 1,
-        backgroundColor: "rgba(21, 21, 21, 0.98)"
-    },
-    title: {
-        fontSize: 30,
-        fontWeight: "bold",
-        color: "white",
-        textAlign: "center",
-        marginVertical: 40,
-    },
-    descriptionCta: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize: 16,
-        marginTop: 20,
-        MarginBottom: 10,
-    },
-    description: {
-        color: "white",
-        textAlign: "white",
-        MarginBottom: 30,
-
-    },
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 40,
+  },
+  descriptionCta: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  description: {
+    color: "white",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+});
